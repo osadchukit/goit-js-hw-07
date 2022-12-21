be1ff29be36c1d7d717763ef20a0c5d11eb6720c
Ivan Osadchuk <osadchukit@gmail.com>
2022-12-21 19:31:27 +0100

    download project

---
+++ src/js/01-gallery.js
@@ -0,0 +1,11 @@
+import { galleryItems } from './gallery-items.js';
+// Change code below this line
+
+console.log(galleryItems);
+
+
+const galleryRef = document.querySelector('.gallery');
+console.log(galleryRef);
+
+
+
