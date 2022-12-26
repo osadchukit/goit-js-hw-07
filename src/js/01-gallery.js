a411994b41113115f550d3e53080efb7e692802d
Ivan Osadchuk <osadchukit@gmail.com>
2022-12-26 15:54:00 +0100

    final

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,11 +1,80 @@
+// import { galleryItems } from './gallery-items.js';
+// // Change code below this line
+
+// const galleryRef = document.querySelector('.gallery');
+
+// const markup = galleryItems.reduce(
+//   (acc, { preview, original, description }) =>
+//     acc +
+//     `<div class="gallery__item">
+//   <a class="gallery__link" href="${original}">
+//     <img
+//       class="gallery__image"
+//       src="${preview}"
+//       data-source="${original}"
+//       alt="${description}"
+//     />
+//   </a>
+// </div>`,
+//   ''
+// );
+
+// galleryRef.insertAdjacentHTML('beforeend', markup);
+
+// galleryRef.addEventListener('click', pictureFull);
+
+// function pictureFull(event) {
+//   event.preventDefault();
+// //   if (!event.target.classList.contains('gallery__image')) return;
+//   if (event.target.nodeNeme !== 'IMG') return;
+
+//   const action = event.target.dataset.source;
+
+//   // console.log(event.target.dataset.source);
+//   // const instance = basicLightbox.create(action, true);
+
+//   const img = basicLightbox.create('<h1>Dynamic Content</h1>', );
+
+// }
+
+// ------------------
+
 import { galleryItems } from './gallery-items.js';
-// Change code below this line
 
-console.log(galleryItems);
+const list = document.querySelector('.gallery');
 
+const markup = galleryItems.reduce(
+  c
 
-const galleryRef = document.querySelector('.gallery');
-console.log(galleryRef);
+list.insertAdjacentHTML('afterbegin', markup);
 
+list.addEventListener('click', clickOnImg);
 
+function clickOnImg(event) {
+  event.preventDefault();
 
+  if (!event.target.classList.contains('gallery__image')) return;
+
+  const options = {
+    onShow: () => {
+      window.addEventListener('keydown', closeESC);
+    },
+    onClose: () => {
+      window.removeEventListener('keydown', closeESC);
+    },
+  };
+
+  const instance = basicLightbox.create(
+    `<img src = "${event.target.dataset.source}">`,
+    options
+  );
+
+  instance.show();
+
+  function closeESC(event) {
+    if (event.code === 'Escape') {
+       instance.close();
+    }
+   
+  }
+}
--- src/js/02-lightbox.js
+++ src/js/02-lightbox.js
@@ -2,3 +2,24 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const markup = galleryItems.reduce(
+  (acc, { original, preview, description }) =>
+    acc +
+    `<a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}"/>
+</a>`,
+  ''
+);
+
+const galleryRef = document.querySelector('.gallery');
+console.log(galleryRef);
+
+galleryRef.insertAdjacentHTML('beforeend', markup);
+
+const options = {
+  captionsData: 'alt',
+  captionDelay: 250,
+};
+
+let lightbox = new SimpleLightbox('.gallery__item', options);
