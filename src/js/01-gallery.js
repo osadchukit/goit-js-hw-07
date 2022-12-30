81b7755fb0850746dfe61c7d5dca293fc336a57a
Ivan Osadchuk <osadchukit@gmail.com>
2022-12-30 23:07:13 +0100

    final

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -44,7 +44,20 @@ import { galleryItems } from './gallery-items.js';
 const list = document.querySelector('.gallery');
 
 const markup = galleryItems.reduce(
-  c
+  (acc, { preview, original, description }) =>
+    acc +
+    `<div class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</div>`,
+  ''
+);
 
 list.insertAdjacentHTML('afterbegin', markup);
 
